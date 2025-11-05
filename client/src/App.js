4499858ab18c00e5cf0d75e4623437282625f513
Ghassen Hammami <161251172+Hammami-Ghassen@users.noreply.github.com>
2025-11-06 00:45:14 +0100

    Add AdminDashboard component to App

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage.jsx';
 import Login from './Pages/Login.jsx';
 import Register from './Pages/Register.jsx';
+import AdminDashboard from './Pages/AdminDashboard.jsx';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <AdminDashboard/>
   );
 }
 
