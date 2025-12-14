c547b03b19d41b6a19259c53d3fb04010401e496
Hafaiedh-Habib <hafaiedh.habib11@gmail.com>
2025-12-14 13:22:24 +0100

    create offers Model

--- client/src/components/DeveloperProfile.jsx
+++ client/src/components/DeveloperProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../Styles/DeveloperProfile.css";
+import "../Styles/DeveloperProfile.module.css";
 
 const DeveloperProfile = ({ userId }) => {
   const [developer, setDeveloper] = useState(null);
